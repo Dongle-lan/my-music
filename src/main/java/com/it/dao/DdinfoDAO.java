060a4a85e698252ac0c0b59c6ea7706cd4bd3ab1
laity <1742764617@qq.com>
2022-05-03 10:45:00 +0800

    first project

---
+++ src/main/java/com/it/dao/DdinfoDAO.java
@@ -0,0 +1,12 @@
+package com.it.dao;
+
+import com.it.entity.Ddinfo;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface DdinfoDAO {
+	List<Ddinfo> selectAll(HashMap map);
+	void add(Ddinfo ddinfo);
+	void delete(int id);
+}
---
+++ src/main/java/com/it/dao/DzrecordDAO.java
@@ -0,0 +1,12 @@
+package com.it.dao;
+
+import com.it.entity.Dzrecord;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface DzrecordDAO {
+	List<Dzrecord> selectAll(HashMap map);
+	void add(Dzrecord dzrecord);
+	void delete(int id);
+}
